7a912e85e7645c9a57c7938a3fd9e38d07727b8f
epsilonwolf <jfengg3@gmail.com>
2024-10-21 22:37:47 +0800

    third

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-import project1 from "../assets/projects/p2p.PNG";
-import project2 from "../assets/projects/rp.PNG";
+import project1 from "../assets/projects/p2p.png";
+import project2 from "../assets/projects/rp.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
